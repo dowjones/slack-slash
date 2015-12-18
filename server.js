2718d80d7f9bdc0a1e8a7f6ad9e659085d31b16f
Adam Gruber <talknmime@gmail.com>
2015-12-18 12:35:27 -0500

    eslint; public handler dirs; delayed response

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-var express      = require('express'),
-    path         = require('path'),
-    logger       = require('morgan'),
-    cookieParser = require('cookie-parser'),
-    bodyParser   = require('body-parser'),
-    indexHandler = require('./routes/index');
-
+var express      = require('express');
+var path         = require('path');
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var indexHandler = require('./routes/index');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -29,7 +28,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.send({
       message: err.message,
@@ -40,15 +39,13 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.send({
     message: err.message,
     error: {}
   });
 });
-
-
 
 app.listen(app.get('port'), function() {
   console.log('slack-slash started on port ' + app.get('port'));
