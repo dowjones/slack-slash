bf605e300170f607d72376feddfef8bd13e140f3
Adam Gruber <talknmime@gmail.com>
2015-12-18 12:35:27 -0500

    eslint; public handler dirs; delayed response

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
-var express  = require('express'),
-    path     = require('path'),
-    nconf    = require('nconf'),
-    router   = express.Router(),
-    handlers = require('../handlers');
-    handlersObj = {};
+var express  = require('express');
+var path     = require('path');
+var fs       = require('fs');
+var _        = require('lodash');
+var request  = require('request');
+var nconf    = require('nconf');
+var router   = express.Router();
+var handlers = require('../handlers');
+var handlersObj = {};
 
 var ERRORS = {
   NOT_FOUND: 'Command Not Found',
   UNAUTHORIZED: 'Unauthorized Request'
 };
+
+/* CREATE PUBLIC DIRECTORY IF IT DOESN'T EXIST */
+var publicPath = path.join(__dirname, '..', 'public');
+createPath(publicPath);
 
 module.exports = router;
 
@@ -34,9 +41,9 @@ handlers.forEach(registerHandler);
 /* FUNCTIONS */
 
 function handleSlackRequest (req, res, next) {
-  var command = req.params ? req.params.command : null,
-      slackToken = req.body ? req.body.token : null,
-      err;
+  var command = req.params ? req.params.command : null;
+  var slackToken = req.body ? req.body.token : null;
+  var err;
 
   // Verify command exists
   if (!command || !handlersObj[command]) {
@@ -54,19 +61,57 @@ function handleSlackRequest (req, res, next) {
 
   // Handle Command
   console.log('Handling command: ' + command);
-  handlersObj[command].handle(req, function(err, message) {
-    if (err) {
-      return res.send(err);
-    }
-    return res.send(message);
+  handlersObj[command].handle(req, function (handlerError, handlerResponse) {
+    respondToSlackRequest(req, res, handlerError, handlerResponse);
   });
 }
 
+function respondToSlackRequest(req, res, handlerError, handlerResponse) {
+
+  console.log('handlerResponse:');
+  console.log(handlerResponse);
+
+  var defaultResponseObj = {
+    response_type: 'ephemeral'
+  };
+  var resObj;
+  var isDelayedResponse;
+
+  // Handle errors and string responses
+  if (handlerError || typeof handlerResponse === 'string') {
+    resObj = _.assign({text: handlerError ? handlerError : handlerResponse}, defaultResponseObj);
+  } else {
+    // Handle response objects
+    isDelayedResponse = handlerResponse.isDelayedResponse;
+    delete handlerResponse.isDelayedResponse;
+    resObj = _.assign({}, defaultResponseObj, handlerResponse);    
+  }
+
+  if (isDelayedResponse) {
+    request({
+      url: req.body.response_url,
+      method: 'POST',
+      body: resObj,
+      json: true
+    }, function () {
+      res.end();
+    });
+  } else {
+    res.json(resObj);
+  }
+}
+
 function registerHandler (handler) {
-  var command = handler.command,
-      pkg     = handler.pkg,
-      token   = nconf.get(handler.tokenVar),
-      opts    = handler.options;
+  var command = handler.command;
+  var pkg = handler.pkg;
+  var token = nconf.get(handler.tokenVar);
+  var opts = handler.options;
+  var dir = path.join(publicPath, command);
+
+  createPath(dir);
+
+  opts.ssFilePath = dir;
+  opts.ssPublicPath = command;
 
   try {
     var SlashCommandHandler = require(pkg);
@@ -74,5 +119,13 @@ function registerHandler (handler) {
   }
   catch (e) {
     console.log(e);
+  }
+}
+
+function createPath(path) {
+  try {
+    fs.statSync(path);
+  } catch (e) {
+    fs.mkdirSync(path);
   }
 }
